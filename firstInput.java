a8b54f97ce0e1daab8d224604c926aff5ae9f1e5
Manishankar-official <manishankargoswami99@gmail.com>
2023-11-12 21:40:32 +0530

    Taking input using Scanner Class!

---
+++ firstInput.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+class Input{
+
+public static void main(String []args){
+  int age ;
+  Scanner sc = new Scanner(System.in);
+  System.out.print("What is your name??    : ");
+  String name = sc.nextLine();
+  System.out.println("Well!! Hello "+name);
+}
+
+
+
+}
